38e7cba5e74a66722e226abd392f503753e1ed23
Sonali Liyana Badalge <sliyanab23@univ.yo.oulu.fi>
2025-02-01 17:01:47 +0200

    adding manipulating js file

--- DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,7 @@
 document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML = 'I changed this text';
+document.querySelector("h1").style.color = "red";
+document.getElementById("but"). style.color = "green";
+document.getElementById("but").innerHTML = "submit"
+document.querySelector(".list a").style.color = "yellow";
+document.getElementsByTagName("button")[0].style.backgroundColor = "blue";
+document.getElementsByClassName("list")[1].style.backgroundColor = "pink";
